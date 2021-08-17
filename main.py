5b39368ac11d142a93d5b1c97ac37997970429c9
LLHill <hieupro.st@gmail.com>
2021-08-17 10:20:41 +0700

    'create project'

---
+++ main.py
@@ -0,0 +1,4 @@
+from pipeline import Pipeline
+
+if __name__ == '__main__':
+        Pipeline.run('D:\job\dataset\ks-projects.csv')
---
+++ transform/adjusted_data_transform.py
@@ -0,0 +1,2 @@
+index_list_to_remove = []
+column_list_to_remove = ['Unnamed: 0','region_1', 'region_2', 'taster_twitter_handle', 'designation']
